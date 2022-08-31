0769ff729cfe6610a21976051fc9c0eba0ef3ff6
RohitKV1n0d <rohitvinod92@gmail.com>
2022-08-31 14:43:07 +0530

    heroku setup

--- app.py
+++ app.py
@@ -337,8 +337,10 @@ def logout():
     logout_user()
     return redirect(url_for('login'))
 
+# if __name__=='__main__':
+#     app.run(debug=True,host='0.0.0.0',port=5000)
 if __name__=='__main__':
-    app.run(debug=True,host='0.0.0.0',port=5000)
+    app.run(debug=True)
 
 
 
